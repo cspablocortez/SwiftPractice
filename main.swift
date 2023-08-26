87c6e88f135a6de078c013234722d7146c7cd7c3
Pablo Cortez <cspablocortez@gmail.com>
2023-08-25 18:00:48 -0700

    Update main.swift

--- main.swift
+++ main.swift
@@ -6,3 +6,10 @@ for character in characters {
     print("Character:  \(character)")
 }
 
+// Fahrenheit - Celsius Converter
+
+let fahr = 98.0
+let celsius = 5 / 9 * (fahr - 32)
+print(celsius)
+
+
